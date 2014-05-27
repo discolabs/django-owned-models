81f58b7309a6e9c51e98e204556fe0a1ae43acc8
Gavin Ballard <gavin@gavinballard.com>
2014-05-27 21:25:59 +0200

    Tweaks to packaging.

--- setup.py
+++ setup.py
@@ -1,33 +1,34 @@
 from distutils.core import setup
 
-version=__import__('owned_models').__version__
+version = __import__('owned_models').__version__
 
 setup(
-    name='django-owned-models',
-    version=version,
-    description='A package providing conveniences around the ownership of models by users.',
-    long_description=open('README.rst').read(),
-    author='Gavin Ballard',
-    author_email='gavin@gavinballard.com',
-    url='https://github.com/gavinballard/django-owned-models',
-    license='MIT',
+    name = 'django-owned-models',
+    version = version,
+    description = 'A package providing conveniences around the ownership of models by users.',
+    long_description = open('README.rst').read(),
+    author = 'Gavin Ballard',
+    author_email = 'gavin@discolabs.com',
+    url = 'https://github.com/discolabs/django-owned-models',
+    license = 'MIT',
 
-    packages=['owned_models'],
-    package_dir={
+    packages = [
+        'owned_models'
+    ],
+
+    package_dir = {
         'owned_models': 'owned_models',
     },
 
-    requires=['Django (>=1.3)',],
-    install_requires=['Django>=1.3',],
-
-    zip_safe=True,
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+    requires = [
+        'django',
+        'tastypie',
     ],
+
+    install_requires = [
+        'django',
+    ],
+
+    zip_safe = True,
+    classifiers = [],
 )
