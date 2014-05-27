ab35bc1912ea580a02a59ca7f4e20111dde53585
Gavin Ballard <gavin@gavinballard.com>
2014-05-27 13:31:12 +0200

    Add `get_for_user` method.

--- owned_models/models.py
+++ owned_models/models.py
@@ -7,6 +7,11 @@ class UserOwnedModelManager(models.Manager):
     def filter_for_user(self, user):
         return super(UserOwnedModelManager, self).get_queryset().filter(user = user)
 
+    def get_for_user(self, user, *args, **kwargs):
+        if 'user' in kwargs:
+            kwargs.pop('user')
+        return super(UserOwnedModelManager, self).get_queryset().get(user = user, *args, **kwargs)
+
 
 class UserOwnedModel(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, editable = False)
